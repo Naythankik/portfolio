252e1a3f523f69e840e714852858aeacd45062a9
naythankik <dolapokiki3ird@gmail.com>
2023-01-29 23:46:25 +0100

    The backend

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const Joi = require("joi");
+require("dotenv");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/sendMail", (req, res) => {
+  const schema = Joi.object({
+    fullName: Joi.string().required(),
+    email: Joi.string().required().email(),
+    message: Joi.string().min(10).required(),
+  });
+
+  const { value, error } = schema.validate(req.body);
+
+  //if there is error,
+  if (error) {
+    const message = error.details[0].message;
+    res.status(400).send({ Error: message });
+    return;
+  }
+
+  res.status(200).send({ success: "Message has been sent" });
+  return;
+});
+
+app.listen(3000, function () {
+  console.log("App is running");
+});
